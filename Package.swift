bb18edac1fd1a588962fde7e37867cf524b26588
etercode <etercodestudio@gmail.com>
2020-09-18 11:33:22 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SimpleImageViewer",
-    dependencies : []
+    platforms: [
+          .iOS(.v12),
+          .tvOS(.v14),
+          .macOS(.v10_15),
+    ],
+    products: [
+        .library(
+            name: "SimpleImageViewer",
+            targets: ["SimpleImageViewer"]),
+    ],
+    targets: [
+        .target(name: "SimpleImageViewer")
+    ],
+    swiftLanguageVersions: [.v5]
 )
